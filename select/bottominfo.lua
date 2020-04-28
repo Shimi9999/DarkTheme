7240673f0f0844f035e7dc603c92c79db81f9b46
Shimi9999 <moguranian999@gmail.com>
2020-04-29 02:32:29 +0900

    BottomInfo: Add IR connect state display and some adjusts

--- select/bottominfo.lua
+++ select/bottominfo.lua
@@ -8,6 +8,15 @@ function BottomInfo.image()
     {id = "uptime", src = src, x = x, y = y + h * 0, w = w, h = h}
   }
 end
+function BottomInfo.text()
+  return {
+    {id = "bottom_fps", font = 1, size = 64, constantText = "FPS"},
+    {id = "bottom_uptime", font = 1, size = 64, constantText = "UPTIME"},
+
+    {id = "ir_offline", font = 1, size = 64, constantText = "IR OFFLINE"},
+    {id = "ir_online", font = 1, size = 64, constantText = "IR ONLINE"}
+  }
+end
 function BottomInfo.value()
   return {
     utils.generateValue({id = "fps_num", digit = 4, ref = 20}),
@@ -19,18 +28,23 @@ function BottomInfo.value()
 end
 function BottomInfo.destination()
   local scale = 0.45
-  local base_x = 4 local y = 4 local nw = 48 * scale local h = 64 * scale
-  local uptime_x = base_x + 140 local uptimenum_x = uptime_x + 76
+  local base_x = 16 local y = 4 local ny = y + 3 local nw = 48 * scale local h = 64 * scale
+  local uptime_x = base_x + 170 local uptimenum_x = uptime_x + 70
+  local ir_x = uptimenum_x + 250
   local info_color = {r = 105, g = 105, b = 105}
   return {
-    {id = "fps_num", dst = { utils.mergeMap({x = base_x, y = y, w = nw, h = h}, info_color) }},
+    {id = "bottom_fps", filter = 1, dst = { utils.mergeMap({x = base_x, y = y, w = nw, h = h}, info_color) }},
+    {id = "fps_num", filter = 1, dst = { utils.mergeMap({x = base_x + 50, y = ny, w = nw, h = h}, info_color) }},
 
-    {id = "uptime", dst = { utils.mergeMap({x = uptime_x, y = y, w = 526 * scale, h = h}, info_color) }},
-    {id = "uptime_hour_num", dst = { utils.mergeMap({x = uptimenum_x + 50 * 1, y = y, w = nw, h = h}, info_color) }},
-    {id = "colon", dst = { utils.mergeMap({x = uptimenum_x + 32 + 50 * 1, y = y, w = nw, h = h}, info_color) }},
-    {id = "uptime_minute_num", dst = { utils.mergeMap({x = uptimenum_x + 50 * 2, y = y, w = nw, h = h}, info_color) }},
-    {id = "colon", dst = { utils.mergeMap({x = uptimenum_x + 32 + 50 * 2, y = y, w = nw, h = h}, info_color) }},
-    {id = "uptime_second_num", dst = { utils.mergeMap({x = uptimenum_x + 50 * 3, y = y, w = nw, h = h}, info_color) }}
+    {id = "bottom_uptime", filter = 1, dst = { utils.mergeMap({x = uptime_x, y = y, w = 1, h = h}, info_color) }},
+    {id = "uptime_hour_num", filter = 1, dst = { utils.mergeMap({x = uptimenum_x + 50 * 1, y = ny, w = nw, h = h}, info_color) }},
+    {id = "colon", filter = 1, dst = { utils.mergeMap({x = uptimenum_x + 32 + 50 * 1, y = ny, w = nw, h = h}, info_color) }},
+    {id = "uptime_minute_num", filter = 1, dst = { utils.mergeMap({x = uptimenum_x + 50 * 2, y = ny, w = nw, h = h}, info_color) }},
+    {id = "colon", filter = 1, dst = { utils.mergeMap({x = uptimenum_x + 32 + 50 * 2, y = ny, w = nw, h = h}, info_color) }},
+    {id = "uptime_second_num", filter = 1, dst = { utils.mergeMap({x = uptimenum_x + 50 * 3, y = ny, w = nw, h = h}, info_color) }},
+
+    {id = "ir_offline", op = {50}, filter = 1, dst = { utils.mergeMap({x = ir_x, y = y, w = 1, h = h}, info_color) }},
+    {id = "ir_online", op = {51}, filter = 1, dst = { utils.mergeMap({x = ir_x, y = y, w = 1, h = h}, info_color) }}
   }
 end
 
